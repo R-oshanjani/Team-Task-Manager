b20f37e87eef9bbfa3b963202ee009b00c85c9a3
SYED ROSHAN JANI <121988869+R-oshanjani@users.noreply.github.com>
2026-05-07 22:25:40 +0530

    Delete backend directory

--- backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, Project, ProjectMember, Task
-
-admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Project)
-admin.site.register(ProjectMember)
-admin.site.register(Task)
