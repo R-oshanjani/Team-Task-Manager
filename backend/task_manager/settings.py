64ab00453d2a9a1a7cac1dd31cdda824b1592552
syed roshan jani <roshanjani842@gmail.com>
2026-05-07 22:00:40 +0530

    Updated project structure and features

--- backend/task_manager/settings.py
+++ backend/task_manager/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # CORS middleware
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Serve static files in production
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
